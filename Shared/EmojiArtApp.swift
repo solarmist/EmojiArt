f3ee6150ec506676600674bfa922a3de34561494
Joshua Olson <joshua.olson+github@gmail.com>
2020-11-14 15:05:26 -0800

    Initial Commit

---
+++ Shared/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  Shared
+//
+//  Created by Joshua Olson on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
